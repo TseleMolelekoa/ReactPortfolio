2fa4caa85503f9b027a79aea195ff4402b00b5e5
TseleMolelekoa <molelekoa58@gmail.com>
2024-02-29 11:26:03 +0200

    .env file added

--- src/components/ContactPage/index.js
+++ src/components/ContactPage/index.js
@@ -11,13 +11,13 @@ import linkedIn from "../assets/249188_linked in_social network_social media_lin
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnPA0wJ18jdleLYsvck4_r3a9BVSyaXjo",
-  authDomain: "contact-me-d671e.firebaseapp.com",
-  databaseURL: "https://contact-me-d671e-default-rtdb.firebaseio.com",
-  projectId: "contact-me-d671e",
-  storageBucket: "contact-me-d671e.appspot.com",
-  messagingSenderId: "763690874771",
-  appId: "1:763690874771:web:d44fbea7de444f26995a66"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase app
@@ -64,7 +64,7 @@ const ContactPage = () => {
     try {
       // Verify reCAPTCHA response with Google's servers
       const response = await axios.post('https://www.google.com/recaptcha/api/siteverify', {
-        secret: '6LezUIMpAAAAAFyw0R1Si7omRJVnte-zZzm0w5yC', // Replace with your reCAPTCHA secret key
+        secret: process.env.REACT_APP_RECAPTCHA_SECRET_KEY, // 
         response: recaptchaValue,
       });
 
@@ -141,8 +141,8 @@ const ContactPage = () => {
             </div>
             <button type="submit">Send</button>
             <ReCAPTCHA
-                sitekey="6LezUIMpAAAAAB959CAW9TBCenzOvWt88Wqtpdnh"
-                onChange={onChange}
+              sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+              onChange={onChange}
             />
           </form>
           <footer className="footer">
